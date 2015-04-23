84cc7982a6579b48080559972a3de22a736f39e5
mitchwenman <mitchwenman@gmail.com>
2015-04-23 14:50:22 +1000

    proper 3d eye calculation

--- GLUT interlacer/OpenGL with 3D/CameraUtil.cpp
+++ GLUT interlacer/OpenGL with 3D/CameraUtil.cpp
@@ -1,8 +1,17 @@
 #include "CameraUtil.h"
+#include "libs\glm\glm.hpp"
+#include "libs\glm\gtc\matrix_transform.hpp"
 
 Vertex3 CameraUtil::calculateRightEye(Camera cam, double eyeDist)
 {
-	Vertex3 rightEye = cam.eye;
-	rightEye.x += eyeDist;//XXX: wrong testing
+	glm::vec3 leftEye = glm::vec3(cam.eye.x, cam.eye.y, cam.eye.z); //Left eye vector
+	glm::vec3 at = glm::vec3(cam.center.x, cam.center.y, cam.center.z); //AT vector
+	glm::vec3 U = glm::vec3(0, 1, 0); 
+	glm::vec3 F = at - leftEye;
+	glm::vec3 V = glm::cross(glm::normalize(F), glm::normalize(U));
+	glm::vec3 R = leftEye + (V * (float)eyeDist); //Convert to float for template
+
+	Vertex3 rightEye = { R.x, R.y, R.z }; //Put result into return vertex3
+	rightEye.x += eyeDist;
 	return rightEye;
 }
