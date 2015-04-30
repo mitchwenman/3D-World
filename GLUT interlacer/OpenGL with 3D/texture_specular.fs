2a0fbaa369f1a4898a447b97f098def753e76d40
mitchwenman <mitchwenman@gmail.com>
2015-04-30 14:06:39 +1000

    used max instead of if > 0

--- GLUT interlacer/OpenGL with 3D/texture_specular.fs
+++ GLUT interlacer/OpenGL with 3D/texture_specular.fs
@@ -28,12 +28,8 @@ void main()
 		vec3 lightReflection = normalize(reflect(nLightDir, nNorm)); //XXX: may need to normalise first
 		vec3 vertEye = normalize(-position); //Vector from pixel to eye at (0,0,0)
 		float shininess = 4.0;
-		float specularIntensity = pow(dot(vertEye, lightReflection), shininess);
-		if (specularIntensity > 0.0)
-		{
-			specular += gl_LightSource[0].specular * specularIntensity;
-		}
-		
+		float specularIntensity = max(0.0, pow(dot(vertEye, lightReflection), shininess)); //Negative angle shouldn't see specular
+		specular += gl_LightSource[0].specular * specularIntensity;
 
 	}
 	
