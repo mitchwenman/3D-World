9876663fc352a53fcc0e525387c154b2bc832bbf
Mitch Wenman <mitchwenman@gmail.com>
2015-05-08 07:05:51 +1000

    push

--- GLUT interlacer/OpenGL with 3D/Maze.cpp
+++ GLUT interlacer/OpenGL with 3D/Maze.cpp
@@ -65,111 +65,145 @@ std::vector<TangentPolygonWorldObject*> Maze::rayCast(Vertex3 position, double a
 	double posx = position.x - xOffset + WALL_WIDTH / 2.0; 
 	double posz = position.z - zOffset + WALL_DEPTH / 2.0; //Blocks defined around origin. So each block occupies -.5 to .5 in space.
 														   //Easier to offset position than take this into account when ray casting.
-	//Adjust angle - cam angle is 0 at PI / 2
-	double viewAngle = fmod(2 * PI - angle + PI / 2, 2 * PI);
-	//Ray cast from right to left - check horizontal intersections then vertical
-	//Move to next ray when: outside grid or wall is found.
-	//If wall found on horizontal and vertical - compare distances
-	double fovInRadians = GraphicsUtil::degreesToRadians(fov);
-	double leftArc = viewAngle + (fovInRadians / 2); //Create ray arc
-	double rightArc = viewAngle - (fovInRadians / 2);
-	double angleIncrement =  (leftArc - rightArc) / fov;
 	std::map<std::pair<int, int>, TangentPolygonWorldObject*> visibleWalls;
-	for (double ray = rightArc; ray <= leftArc; ray += angleIncrement)
+	// Check bounds
+	if (position.x < xOffset || position.x > xOffset + WALL_WIDTH * columns ||
+		position.z < zOffset || position.z > zOffset + WALL_DEPTH * rows)
 	{
-		double modRay = fmod(ray + 2 * PI, 2 * PI); //Get rid of negatives
-		double rayCalc = modRay;
-		//Adjust ray if outside of first quadrant
-		//Take care of the sign later
-		if (rayCalc < PI && rayCalc > PI / 2)
-			rayCalc = PI / 2 - (rayCalc - PI / 2);
-		else if (rayCalc < 3 * PI / 2 && rayCalc > PI)
-			rayCalc = rayCalc - PI;
-		else if (rayCalc > 3 * PI / 2)
-			rayCalc = 2 * PI - ray;
+		//Render all outside blocks
+		for (unsigned int i = 0; i < maze.size(); i++)
+		{
+			if ((i == 0 && position.z < zOffset) || //Top or bottom
+				(i == maze.size() - 1 && position.z > zOffset + WALL_DEPTH * columns ))
+			{
+				for (unsigned int j = 0; j < maze[i].size(); j++)
+				{
+					if (maze[i][j]) 
+					{
+						std::pair<int, int> coords(i, j);
+						visibleWalls[coords] = walls[coords];
+					}
+				}
+			} 
+			if (maze[i][0] && position.x < xOffset) //left edge
+			{
+				std::pair<int, int> coords(i, 0);
+				visibleWalls[coords] = walls[coords];
+			} else if (maze[i][maze[i].size() - 1] && position.x > xOffset + WALL_WIDTH * rows) //right edge
+			{
+				std::pair<int, int> coords(i, maze[i].size() - 1);
+				visibleWalls[coords] = walls[coords];
+			}
+		}
+	} else
+	{
+		//Adjust angle - cam angle is 0 at PI / 2
+		double viewAngle = fmod(2 * PI - angle + PI / 2, 2 * PI);
+		//Ray cast from right to left - check horizontal intersections then vertical
+		//Move to next ray when: outside grid or wall is found.
+		//If wall found on horizontal and vertical - compare distances
+		double fovInRadians = GraphicsUtil::degreesToRadians(fov);
+		double leftArc = viewAngle + (fovInRadians / 2); //Create ray arc
+		double rightArc = viewAngle - (fovInRadians / 2);
+		double angleIncrement =  (leftArc - rightArc) / fov;
 
-		int xHIntercept = -1;
-		int zHIntercept = -1;
-		double xH = posx;
-		double zH = posz;
-		double xIncrease = .5 / tan(rayCalc); // adj = opp / tan(x)
-		if (modRay < 3 * PI / 2 && modRay >= PI / 2) xIncrease *= -1; // Q2/Q3 - going left
-		double zDiff = (modRay < PI) ? -.5 : .5; //If ray is going down need to increase z
-		//Check for horizontal intersections
-		int scale = 1;
-		while (xH < columns && xH >= 0 &&
-				zH < rows && zH >= 0)
-		{			
-			xH = posx + scale * xIncrease;
-			zH += zDiff;
-			if (walls.count(std::pair<int, int>((int)zH, (int)xH)) == 1) //Check for a wall
-			{						
-				xHIntercept = (int)xH;
-				zHIntercept = (int)zH;
-				break;
+		for (double ray = rightArc; ray <= leftArc; ray += angleIncrement)
+		{
+			double modRay = fmod(ray + 2 * PI, 2 * PI); //Get rid of negatives
+			double rayCalc = modRay;
+			//Adjust ray if outside of first quadrant
+			//Take care of the sign later
+			if (rayCalc < PI && rayCalc > PI / 2)
+				rayCalc = PI / 2 - (rayCalc - PI / 2);
+			else if (rayCalc < 3 * PI / 2 && rayCalc > PI)
+				rayCalc = rayCalc - PI;
+			else if (rayCalc > 3 * PI / 2)
+				rayCalc = 2 * PI - ray;
+
+			int xHIntercept = -1;
+			int zHIntercept = -1;
+			double xH = posx;
+			double zH = posz;
+			double xIncrease = .5 / tan(rayCalc); // adj = opp / tan(x)
+			if (modRay < 3 * PI / 2 && modRay >= PI / 2) xIncrease *= -1; // Q2/Q3 - going left
+			double zDiff = (modRay < PI) ? -.5 : .5; //If ray is going down need to increase z
+			//Check for horizontal intersections
+			int scale = 1;
+			while (xH < columns && xH >= 0 &&
+					zH < rows && zH >= 0)
+			{			
+				xH = posx + scale * xIncrease;
+				zH += zDiff;
+				if (walls.count(std::pair<int, int>((int)zH, (int)xH)) == 1) //Check for a wall
+				{						
+					xHIntercept = (int)xH;
+					zHIntercept = (int)zH;
+					break;
+				}
+				scale++;
 			}
-			scale++;
-		}
 				
-		//Check for vertical intersections
-		int xVIntercept = -1;
-		int zVIntercept = -1;
-		double xV = posx;
-		double zV = posz;
-		double xDiff = (modRay < PI / 2 || modRay > 3 * PI / 2) ? .5 : -.5; // Q2/Q3 going left
-		double zIncrease = .5 * tan(rayCalc); // opp = adj * tan(x)
-		if (modRay < PI) zIncrease *= -1;
-		scale = 1;
-		while (xV < columns && xV >= 0 &&
-				zV < rows && zV >= 0)
-		{
-			xV = posx + scale * xDiff;
-			zV = posz + scale * zIncrease;
-			if (walls.count(std::pair<int, int>((int)zV, (int)xV)) == 1)
-			{				
-				xVIntercept = (int)xV;
-				zVIntercept = (int)zV;
-				break; 
+			//Check for vertical intersections
+			int xVIntercept = -1;
+			int zVIntercept = -1;
+			double xV = posx;
+			double zV = posz;
+			double xDiff = (modRay < PI / 2 || modRay > 3 * PI / 2) ? .5 : -.5; // Q2/Q3 going left
+			double zIncrease = .5 * tan(rayCalc); // opp = adj * tan(x)
+			if (modRay < PI) zIncrease *= -1;
+			scale = 1;
+			while (xV < columns && xV >= 0 &&
+					zV < rows && zV >= 0)
+			{
+				xV = posx + scale * xDiff;
+				zV = posz + scale * zIncrease;
+				if (walls.count(std::pair<int, int>((int)zV, (int)xV)) == 1)
+				{				
+					xVIntercept = (int)xV;
+					zVIntercept = (int)zV;
+					break; 
+				}
+				scale++;
 			}
-			scale++;
-		}
 		
-		if (xVIntercept == -1 && xHIntercept != -1) //No vertical intercept
-		{
-			std::pair<int, int> coords(zHIntercept, xHIntercept);
-			visibleWalls[coords] = walls[coords];
-		} else if (xHIntercept == -1 && xVIntercept != -1) //No horizontal intercept
-		{
-			std::pair<int, int> coords(zVIntercept, xVIntercept);
-			visibleWalls[coords] = walls[coords];
-		} else if (xHIntercept != -1 && xVIntercept != -1) //Compare distance
-		{
-			double xHDist = xHIntercept - posx;
-			double zHDist = zHIntercept - posz;
-			double hDistance = sqrt(xHDist * xHDist + zHDist * zHDist);
-			double xVDist = xVIntercept - posx;
-			double zVDist = zVIntercept - posz;
-			double vDistance = sqrt(xVDist * xVDist + zVDist * zVDist);
-			std::pair<int, int> coords;
-			if (hDistance < vDistance)
+			if (xVIntercept == -1 && xHIntercept != -1) //No vertical intercept
 			{
-				coords = std::pair<int, int>(zHIntercept, xHIntercept);
+				std::pair<int, int> coords(zHIntercept, xHIntercept);
 				visibleWalls[coords] = walls[coords];
-			} else if (vDistance < hDistance)
+			} else if (xHIntercept == -1 && xVIntercept != -1) //No horizontal intercept
 			{
-				coords = std::pair<int, int>(zVIntercept, xVIntercept);
+				std::pair<int, int> coords(zVIntercept, xVIntercept);
 				visibleWalls[coords] = walls[coords];
-			} else // Both are the same distance away (probably same coords), so add both to the list. 
+			} else if (xHIntercept != -1 && xVIntercept != -1) //Compare distance
 			{
-				std::pair<int, int> coords = std::pair<int, int>(zHIntercept, xHIntercept);
-				visibleWalls[coords] = walls[coords];
-				coords = std::pair<int, int>(zVIntercept, xVIntercept);
-				visibleWalls[coords] = walls[coords];
+				double xHDist = xHIntercept - posx;
+				double zHDist = zHIntercept - posz;
+				double hDistance = sqrt(xHDist * xHDist + zHDist * zHDist);
+				double xVDist = xVIntercept - posx;
+				double zVDist = zVIntercept - posz;
+				double vDistance = sqrt(xVDist * xVDist + zVDist * zVDist);
+				std::pair<int, int> coords;
+				if (hDistance < vDistance)
+				{
+					coords = std::pair<int, int>(zHIntercept, xHIntercept);
+					visibleWalls[coords] = walls[coords];
+				} else if (vDistance < hDistance)
+				{
+					coords = std::pair<int, int>(zVIntercept, xVIntercept);
+					visibleWalls[coords] = walls[coords];
+				} else // Both are the same distance away (probably same coords), so add both to the list. 
+				{
+					std::pair<int, int> coords = std::pair<int, int>(zHIntercept, xHIntercept);
+					visibleWalls[coords] = walls[coords];
+					coords = std::pair<int, int>(zVIntercept, xVIntercept);
+					visibleWalls[coords] = walls[coords];
+				}
+			
 			}
 			
 		}
-			
 	}
+
 	std::vector<TangentPolygonWorldObject *> visibleWallList;
 	// Build list of the walls that are visible.
 	for (std::map<std::pair<int, int>, TangentPolygonWorldObject*>::iterator it = visibleWalls.begin(); it != visibleWalls.end(); it++)
